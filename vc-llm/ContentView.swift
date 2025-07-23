502dc0e3af5e46a1cef5b4144c7a644810d71f12
ronin207 <takumi.ot0911@gmail.com>
2025-07-23 16:07:15 +0900

    Initial Commit

---
+++ vc-llm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vc-llm
+//
+//  Created by Takumi Otsuka on 2025/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vc-llm/vc_llmApp.swift
@@ -0,0 +1,17 @@
+//
+//  vc_llmApp.swift
+//  vc-llm
+//
+//  Created by Takumi Otsuka on 2025/07/23.
+//
+
+import SwiftUI
+
+@main
+struct vc_llmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
